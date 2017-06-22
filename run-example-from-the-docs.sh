601526d3239fbe9a81e86cbb26e95f24f195e9d9
loveencounterflow <paragate@gmx.net>
2017-06-22 12:36:06 +0200

    add example scripts

---
+++ run-example-from-the-docs.sh
@@ -0,0 +1,8 @@
+
+
+nearleyc grammars/json.ne -o grammars/json.js && node from-the-docs.js
+
+
+
+
+
---
+++ run-example-with-nearley-test.sh
@@ -0,0 +1,8 @@
+
+
+nearleyc grammars/json.ne -o grammars/json.js && nearley-test grammars/json.js -i "[1,2,3]"
+
+
+
+
+
